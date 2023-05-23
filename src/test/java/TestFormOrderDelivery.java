becdf25e46c4f3e41e970d1fc091077db75af76d
Olga Melman <olga.melman198237@gmail.com>
2023-05-24 01:10:26 +0300

    added sendKeys into test

--- src/test/java/TestFormOrderDelivery.java
+++ src/test/java/TestFormOrderDelivery.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -11,18 +12,19 @@ public class TestFormOrderDelivery {
     private String generateData(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
+
     @Test
     void shouldPositivePathForOrderDeliveryCard() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Оренбург");
-        String currentDay = generateData(3, "dd.MM.yyyy");
-        $("[data-test-id=date] input").setValue(currentDay);
+        String currentDay = generateData(7, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(currentDay);
         $("[data-test-id=name] input").setValue("Петров Иван");
         $("[data-test-id=phone] input").setValue("+79226789877");
         $("[data-test-id=agreement]").click();
         $x("//*[contains(text(),'Забронировать')]").click();
-        $x("//*[contains(text(),'Успешно')]").should(Condition.appear, Duration.ofSeconds(15));
-        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + currentDay), Duration.ofSeconds(15));
-
+        $("[data-test-id=notification] .notification__content").should(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + currentDay));
     }
 }
